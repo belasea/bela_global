e94ee81651f96de7e5a0eb402bad7b9e6d685a9f
Belasea Developer <developerbelasea.com>
2026-05-10 10:51:42 +0600

    change local settings.py file and others

--- the_bella/local_settings.py
+++ the_bella/local_settings.py
@@ -1,22 +1,35 @@
 
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = '2abbb66e6262d8a471b69c7jayedhossainjibona03a0c74bjibon969'
+SECRET_KEY = '2abbb66e6262d8a471b69c7jayedhossainjibona03a0c74bjibon69'
 
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "http://143.198.167.62"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Django Settings.py
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'the_bela_db',
+       'USER': 'postgres',
+       'PASSWORD': 'bela355168AmDB',
+       'HOST': '127.0.0.1',
+       'PORT': '5432',
+   }
 }
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
